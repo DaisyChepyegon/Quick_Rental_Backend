d02266b2a6c3973132c853b1b48057581eb3f885
DaisyChepyegon <daisychepyegon@gmail.com>
2022-09-08 19:32:06 +0300

    add the get routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,56 @@ class ApplicationController < Sinatra::Base
     { message: "Good luck with your project!" }.to_json
   end
 
+  get '/houses' do
+    houses = House.all.order(:location).limit(20)
+    houses.to_json
+  end
+
+  get '/houses' do
+    houses = House.all.order(:price).limit(20)
+    houses.to_json
+  end
+
+  get '/houses/:id' do
+    houses = House.find(params[:id])
+    
+    houses.to_json(include: :reviews)
+  end
+
+  get '/reviews' do
+    reviews = Review.all.order(:name).limit(20)
+    reviews.to_json
+  end
+
+  post '/reviews' do
+    review = Review.create(
+      name: params[:name],
+      comment: params[:comment],
+      house_id: params[:house_id]
+    )
+    review.to_json
+  end
+
+  post '/houses' do
+    house = House.create(
+      location: params[:location],
+      bedrooms: params[:bedrooms],
+      price: params[:price],
+      landload_id: params[:landload_id],
+      description: params[:description]
+    )
+  end
+
+  delete '/houses/:id' do
+    houses = House.find(params[:id])
+    houses.destroy
+    houses.to_json
+  end
+
+  delete '/reviews/:id' do
+    review = Review.find(params[:id])
+    review.destroy
+    review.to_json
+  end
+
 end
